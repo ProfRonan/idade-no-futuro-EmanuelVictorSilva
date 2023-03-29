74f4bffad179f2522e93dbc47badf5854f2dcb8b
EmanuelVictorSilva <emanuelvictorsilva768@gmail.com>
2023-03-29 11:49:55 -0300

    Atividade concluída

---
+++ main.py
@@ -0,0 +1,6 @@
+x=int(input("Digite o ano atual: "))
+z=int(input("Digite um ano"))
+y=int(input("Digite a sua idade:"))
+a=input("Digite seu nome:")
+
+print(f"{a}, no ano de {z} você terá {z-x+ y} anos")
