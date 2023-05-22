9ad0bcaaadcc37f382b141b68362aaaf19498852
Ronaldo Arrais <arraisronaldo6@gmail.com>
2023-05-22 11:00:14 -0300

    Exercício Concluído

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 x=int(input("Digite o ano atual: "))
-z=int(input("Digite um ano"))
-y=int(input("Digite a sua idade:"))
+y=int(input("Digite a sua idade:"))      
+z=int(input("Digite um ano:"))                    
 a=input("Digite seu nome:")
 
 print(f"{a}, no ano de {z} você terá {z-x+ y} anos")
+#digita 2010, 15, 2023, Fulano
+    
